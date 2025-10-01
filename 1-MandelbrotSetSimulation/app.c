c5e0c308b7a05367af9b6ad4845b5a4ae9b6e7a1
Владислав Витальевич <vlad@MacBook-Pro.local>
2025-10-02 00:22:31 +0300

    update zooming

--- 1-MandelbrotSetSimulation/app.c
+++ 1-MandelbrotSetSimulation/app.c
@@ -1,44 +1,29 @@
 #include "sim.h"
 
-#include <stdint.h>
-#include <math.h>
+#define MAX_ITERATIONS 50
 
-#define MAX_ITERATIONS 100
-
-static double mandelbrotBuf[SIM_Y_SIZE][SIM_X_SIZE];
 static int colorBuf[SIM_Y_SIZE][SIM_X_SIZE];
 
-static double centerX = -0.5;
-static double centerY = 0.0;
-static double scale = 3.0;
-
-static int calcMandelbrot(int cx, int cy, int scaleShift) {
+static int calcMandelbrot(int cx, int cy) {
     int x = 0, y = 0;
-    int x2 = 0, y2 = 0;
     int iter = 0;
     
+    const int SCALE = 1000;
+    
     while (iter < MAX_ITERATIONS) {
-        // Проверяем условие |z| < 2
-        if (x2 + y2 > (4 << (2 * scaleShift))) {
-            break;
-        }
-            
-        // Вычисляем следующую итерацию: z = z² + c
-        // Для целых чисел: xNew = x² - y² + cx
-        //                  yNew = 2*x*y + cy
-        int xNew = ((x2 - y2) >> scaleShift) + cx;
-        int yNew = ((2 * ((x * y) >> scaleShift)) >> scaleShift) + cy;
+        int x2 = (x * x) / SCALE;
+        int y2 = (y * y) / SCALE;
         
-        x = xNew;
-        y = yNew;
+        // Условие выхода |z| > 2
+        if (x2 + y2 > 4 * SCALE) break;
         
-        // Вычисляем квадраты для следующей итерации
-        x2 = (x * x) >> scaleShift;
-        y2 = (y * y) >> scaleShift;
+        int temp = (x2 - y2) + cx;
+        y = (2 * x * y) / SCALE + cy;
+        x = temp;
         
         iter++;
     }
-
+    
     return iter;
 }
 
@@ -46,24 +31,20 @@ static int iter2Color(int iter) {
     if (iter == MAX_ITERATIONS)
     { return 0xFF000000; }
     
-    int r = (iter * 8) % 256;
-    int g = (iter * 5) % 256;
+    int r = (iter * 12) % 256;
+    int g = (iter * 7) % 256;
     int b = (iter * 3) % 256;
     
     return 0xFF000000 | (r << 16) | (g << 8) | b;
 }
 
-static void calcFrame(void) {
-    int scaleShift = 12;
-    int centerX = -2000;
-    int scale = 4000;
-    
+static void calcFrame(int scale, int centerX, int centerY) {
     for (int y = 0; y < SIM_Y_SIZE; ++y) {
         for (int x = 0; x < SIM_X_SIZE; ++x) {
-            int cx = ((x - SIM_X_SIZE / 2) * scale) / SIM_X_SIZE + centerX;
-            int cy = ((y - SIM_Y_SIZE / 2) * scale) / SIM_Y_SIZE;
+            int cx = ((x - SIM_X_SIZE / 2) * scale) / (SIM_X_SIZE * 100) + centerX;
+            int cy = ((y - SIM_Y_SIZE / 2) * scale) / (SIM_Y_SIZE * 100) + centerY;
             
-            int iter = calcMandelbrot(cx, cy, scaleShift);
+            int iter = calcMandelbrot(cx, cy);
             colorBuf[y][x] = iter2Color(iter);
         }
     }
@@ -79,59 +60,28 @@ static void drawFrame(void) {
 }
 
 void app(void) {
-    // start from
-    int startCenterX = -5000;
+    // start point
+    int startCenterX = -500;
     int startCenterY = 0;
-    int startScale = 10000;
+    int startScale = 400000;
     
-    // end on
-    int endCenterX = -7500;
-    int endCenterY = 1000;
-    int endScale = 100;
+    // end point
+    int endCenterX = -750;
+    int endCenterY = 100;
+    int endScale = 10000;
     
-    const int TOTAL_STEPS = 200;
+    const int TOTAL_STEPS = 500;
     
-    for (int step = 0; step < TOTAL_STEPS; ++step) {
-        int currentCenterX = startCenterX + 
-            ((endCenterX - startCenterX) * step) / TOTAL_STEPS;
+    for (int step = 0; step <= TOTAL_STEPS; ++step) {
+        // zooming
+        int currentCenterX = startCenterX + ((endCenterX - startCenterX) * step) / TOTAL_STEPS;
+        int currentCenterY = startCenterY + ((endCenterY - startCenterY) * step) / TOTAL_STEPS;
+        int currentScale   = startScale   + ((endScale   - startScale  ) * step) / TOTAL_STEPS;
         
-        int currentCenterY = startCenterY + 
-            ((endCenterY - startCenterY) * step) / TOTAL_STEPS;
+        calcFrame(currentScale, currentCenterX, currentCenterY);        
+        drawFrame();
         
-        int progress = step;
-        int currentScale;
-        
-        if (progress < TOTAL_STEPS / 2) {
-            // slow zoom
-            currentScale = startScale - 
-                ((startScale - startScale / 10) * progress) / (TOTAL_STEPS / 2);
-        } else {
-            // fast zoom
-            int fastProgress = progress - TOTAL_STEPS / 2;
-            currentScale = startScale / 10 - 
-                ((startScale / 10 - endScale) * fastProgress) / (TOTAL_STEPS / 2);
-        }
-        
-        // calculate frame
-        for (int y = 0; y < SIM_Y_SIZE; ++y) {
-            for (int x = 0; x < SIM_X_SIZE; ++x) {
-                int cx = ((x - SIM_X_SIZE / 2) * currentScale) / SIM_X_SIZE + currentCenterX;
-                int cy = ((y - SIM_Y_SIZE / 2) * currentScale) / SIM_Y_SIZE + currentCenterY;
-                
-                int iter = calcMandelbrot(cx, cy, currentScale / 100 + 1);
-                colorBuf[y][x] = iter2Color(iter);
-            }
-        }
-        
-        for (int y = 0; y < SIM_Y_SIZE; ++y) {
-            for (int x = 0; x < SIM_X_SIZE; ++x) {
-                simPutPixel(x, y, colorBuf[y][x]);
-            }
-        }
-        simFlush();
-        
-        // Простая задержка
-        for (volatile int i = 0; i < 50000; ++i) {}
+        // delay
+        // for (volatile int i = 0; i < 100000; ++i) {}
     }
 }
-
